cf0e0f55a823b3a120fe10a92e9c67dbd2253d16
soothescribe-ASMR <soothescribe@gmail.com>
2025-09-06 17:52:44 +0530

    Update stitch.py

--- render/stitch.py
+++ render/stitch.py
@@ -1,11 +1,13 @@
 import subprocess, glob, os
-loops = sorted(glob.glob("loops/*.mp4"))
-with open("file_list.txt","w") as f:
+
+loops = sorted(glob.glob("../loops/*.mp4"))
+with open("file_list.txt", "w") as f:
     for l in loops:
         f.write(f"file '{l}'\n")
+
 subprocess.run([
-    "ffmpeg","-f","concat","-safe","0","-i","file_list.txt",
-    "-i","audio/moon_whisper_1hr.wav",
-    "-shortest","-c:v","copy","-c:a","aac","-b:a","128k",
-    "-movflags","+faststart","../SootheScribe_MoonStory_1hr.mp4"
+    "ffmpeg", "-f", "concat", "-safe", "0", "-i", "file_list.txt",
+    "-i", "../audio/moon_whisper_1hr.wav",
+    "-shortest", "-c:v", "copy", "-c:a", "aac", "-b:a", "128k",
+    "-movflags", "+faststart", "../SootheScribe_MoonStory_1hr.mp4"
 ])
